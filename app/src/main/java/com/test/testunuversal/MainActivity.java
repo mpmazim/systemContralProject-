03393a5b4e7ce65726e53dd472eaff875dd95ca5
mpmazim <mpmazim@gmail.com>
2017-06-18 07:15:09 +0530

    initial

--- app/src/main/java/com/test/testunuversal/MainActivity.java
+++ app/src/main/java/com/test/testunuversal/MainActivity.java
@@ -2,15 +2,15 @@ package com.test.testunuversal;
 
 import android.app.ActivityManager;
 import android.app.admin.DevicePolicyManager;
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import java.io.File;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     DevicePolicyManager deviceManger;
@@ -20,22 +20,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        deviceManger = (DevicePolicyManager)getSystemService(
-                Context.DEVICE_POLICY_SERVICE);
-        activityManager = (ActivityManager)getSystemService(
-                Context.ACTIVITY_SERVICE);
-        compName = new ComponentName(this, DeviceAdminLock.class);
 
-        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
-        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN,compName);
-        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,"Additional text explaining why this needs to be added.");
-        startActivityForResult(intent, 1);
-        boolean active = deviceManger.isAdminActive(compName);
-        if (active) {
-// if available then lock
-            deviceManger.lockNow();
-        }
-
-        deviceManger.removeActiveAdmin(compName);
+TextView textView= (TextView) findViewById(R.id.textView1);
+       textView.setTag("asss");
+        textView.setOnTouchListener(new dragTouchListener());
+        //textView.setOnDragListener(new dropListener());
     }
 }
+  class dragTouchListener implements View.OnTouchListener {
+      float dX, dY;
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+
+                dX = v.getX() - event.getRawX();
+                dY = v.getY() - event.getRawY();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                v.animate()
+                        .x(event.getRawX() + dX)
+                        .y(event.getRawY() + dY)
+                        .setDuration(0)
+                        .start();
+                break;
+            default:
+                return false;
+        }
+        return true;
+        }
+    }
+
+
